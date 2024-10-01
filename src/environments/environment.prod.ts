316f8694f63926ea71ff5199b2a53fe6b6d3f6c6
Urrix <mauribarrien@gmail.com>
2024-09-30 18:52:29 -0600

    firebase/hosting

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDAvfFtxM9gUjy4NFoRZ8WGHwDfeWxwNak",
+    authDomain: "galleryphotos-8bbfe.firebaseapp.com",
+    projectId: "galleryphotos-8bbfe",
+    storageBucket: "galleryphotos-8bbfe.appspot.com",
+    messagingSenderId: "1045038429374",
+    appId: "1:1045038429374:web:629231c8e58ddf3c791aef",
+    measurementId: "G-QWQ2J5W1W9"
+  }
+};
